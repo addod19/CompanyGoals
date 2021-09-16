03c29975a49ff24a080dd14cc499fee1b2f3019a
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2021-09-16 19:49:49 +0000

    Fix rubocop errors

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,8 +1,6 @@
 class Goal < ApplicationRecord
   belongs_to :parent, class_name: :Goal, foreign_key: :parent_id
   has_many :children, class_name: :Goal, foreign_key: :parent_id
-
-
   validates :title, presence: true
   validates :progress, presence: true
 end
