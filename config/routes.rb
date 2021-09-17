e82ef0dab4f95ad578b85cf35adecffad6fe2d44
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2021-09-17 13:34:58 +0000

    Remove comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :goals
-  # get '/show_goal', to: 'goals#show'
-  # patch '/update_goal', to: 'goals#update'
 end
