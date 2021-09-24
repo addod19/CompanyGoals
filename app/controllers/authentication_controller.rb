4553f730dc737f3c2ad1302b258c8e056de35c2d
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2021-09-24 17:38:33 +0000

    Add authentication controller to handle login actions

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,15 @@
+class AuthenticationController < ApplicationController
+  skip_before_action :authenticate_request
+
+  def authenticate
+    user = User.find_by(email: params[:email])
+
+    auth = AuthenticateUser.call(params[:email], params[:password])
+
+    if auth.success?
+      render json: { auth_token: auth.result, user: user }
+    else
+      render json: { error: auth.errors.full_messages }, status: :unauthorized
+    end
+  end
+end
